d1c1850a8b1f2b013fbaa25be60b2364a2c39989
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-04 12:19:30 +0600

    get product by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -39,6 +39,12 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/products/:id', async(req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await productsCollection.findOne(query)
+      res.send(result)
+    })
     app.get('/products/:category', async(req, res) => {
       const category = req.params.category
       const query = {category: category}
