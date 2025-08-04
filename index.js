63493194f2b202f653fb1f39216a23f32a6d4501
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-04 20:21:53 +0600

    delete product function

--- index.js
+++ index.js
@@ -63,6 +63,14 @@ async function run() {
       res.send(result)
     })
 
+    // Delete Product on Database
+    app.delete('/product/:id', async(req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await productsCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
