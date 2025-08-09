e8bbe6df24c434ae8a38118946affe427d1298fb
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-09 17:40:37 +0600

    axisting user status update

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/users/:email', async(req, res) => {
+    app.get('/users/:email', async (req, res) => {
       const email = req.params.email
-      const query = {email: email}
+      const query = { email: email }
       const result = await usersCollection.findOne(query)
       res.send(result)
     })
